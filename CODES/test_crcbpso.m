3d42b7b312559b459752584a9e1537cd84caf6e7
Soumya Mohanty <Soumya@Soumyas-Mac-Pro.rgv.rr.com>
2020-03-18 17:06:06 -0500

    Repairing accidental deletion.

---
+++ CODES/test_crcbpso.m
@@ -0,0 +1,17 @@
+%% Test harness for CRCBPSO 
+% The fitness function called is CRCBPSOTESTFUNC. 
+ffparams = struct('rmin',-100,...
+                     'rmax',100 ...
+                  );
+% Fitness function handle.
+fitFuncHandle = @(x) crcbpsotestfunc(x,ffparams);
+%%
+% Call PSO.
+rng('default')
+psoOut = crcbpso(fitFuncHandle,2);
+
+%% Estimated parameters
+% Best standardized and real coordinates found.
+stdCoord = psoOut.bestLocation;
+[~,realCoord] = fitFuncHandle(stdCoord);
+disp(realCoord);
