13382818e6d2f8930b8f1735f98fd582877808ba
soumya <soumya.d.mohanty@gmail.com>
2021-05-01 19:27:57 -0500

    Corrected help.

--- CODES/r2sv.m
+++ CODES/r2sv.m
@@ -4,7 +4,7 @@ function xVec = r2sv(rVec,params)
 %Takes real coordinates in R (coordinates of one point per row) and returns
 %standardized coordinates in X using the range limits defined in P.rmin and
 %P.rmax. The range limits can be different for different dimensions. (If
-%they are same for all dimensions, use S2RS instead.)
+%they are same for all dimensions, use R2SS instead.)
 
 %Soumya D. Mohanty
 %May 2016
